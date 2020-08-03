3780ee9b0c80f58aa28e67cf8c64944b4461319b
Tanin Rahman <taninrahman07@gmail.com>
2020-08-03 20:56:28 +0600

    update:new stats

--- app/Http/Controllers/StatsController.php
+++ app/Http/Controllers/StatsController.php
@@ -18,6 +18,8 @@ use App\Brand;
 
 use App\Product;
 
+use App\Purchase;
+
 class StatsController extends Controller
 {
 
@@ -56,23 +58,35 @@ class StatsController extends Controller
                     ->sum('amount');
 
         $monthly_expense=Expense::whereBetween('created_at',[$this->time->copy()->startOfMonth(),$this->time->copy()->endOfMonth()])
-        ->where('employee_id',$employee_id)
-        ->sum('amount');
+                        ->where('employee_id',$employee_id)
+                        ->sum('amount');
 
         $total_products=Product::all()->count();
 
         $total_brands=Brand::all()->count();
 
+        $daily_purchase=Purchase::whereBetween('created_at',[$this->time->copy()->startOfDay(),$this->time->copy()->endOfDay()])
+                        ->sum('cost');
+
+        $weekly_purchase=Purchase::whereBetween('created_at',[$start,$end])
+                        ->sum('cost');
+
+        $monthly_purchase=Purchase::whereBetween('created_at',[$this->time->copy()->startOfMonth(),$this->time->copy()->endOfMonth()])
+                        ->sum('cost');
+
                     
         return response()->json(['data'=>[
             'daily_sale'=>$daily_sale,
             'daily_expense'=>$daily_expense,
+            'daily_purchase'=>$daily_purchase,
             'weekly_sale'=>$weekly_sale,
             'weekly_expense'=>$weekly_expense,
+            'weekly_purchase'=>$weekly_purchase,
             'monthly_sale'=>$monthly_sale,
             'monthly_expense'=>$monthly_expense,
+            'monthly_purchase'=>$monthly_purchase,
             'total_products'=>$total_products,
-            'total_brands'=>$total_brands,
+            'total_brands'=>$total_brands
         ]]);
     }
 
